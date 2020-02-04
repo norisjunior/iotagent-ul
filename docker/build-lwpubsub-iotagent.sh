020f2d249287717023f74010e4f90dfabaf42057
norisjunior <norisjunior@gmail.com>
2020-02-04 12:21:48 -0200

    no-cache when building docker image

--- docker/build-lwpubsub-iotagent.sh
+++ docker/build-lwpubsub-iotagent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-docker build -t lwpubsub-iotagent . \
+docker build --no-cache -t lwpubsub-iotagent . \
     --build-arg GITHUB_ACCOUNT=norisjunior \
     --build-arg GITHUB_REPOSITORY=iotagent-ul \
     --build-arg SOURCE_BRANCH=lwpubsub-iotagent
