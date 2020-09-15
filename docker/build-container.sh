2618d9012a2a8551c0fbee8816ee22e1222e5758
Ferraz Junior, N <norisjunior@gmail.com>
2020-09-15 09:32:19 -0300

    ccm build

--- docker/build-container.sh
+++ docker/build-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-docker build --no-cache -t norisjunior/lwpubsub-iotagent:v2.0 . \
+docker build --no-cache -t norisjunior/lwpubsub-iotagent-ccm:v2.0 . \
   --build-arg GITHUB_ACCOUNT=norisjunior \
   --build-arg GITHUB_REPOSITORY=iotagent-ul \
   --build-arg SOURCE_BRANCH=origin/lwpubsub-iotagent-ccm
